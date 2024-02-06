4188a23c4b09ac27fb4514e4f4cf367af8388823
PUN-145 <pun-145@hmxmedia.com>
2024-02-06 21:02:41 +0530

    added Floor number code

--- src/test/java/naveen/binary/FloorNumber.java
+++ src/test/java/naveen/binary/FloorNumber.java
@@ -4,16 +4,27 @@ public class FloorNumber {
 
 	public static void main(String[] args) {
 		int[] arr = { 2, 6, 9, 34, 67, 89, 90, 104, 205, 607, 1092 };
+		System.out.println(floor(arr, 88));
 
 	}
 	
-	static void floor(int[] a, int target) {
+	static int floor(int[] a, int target) {
 		int start = 0;
 		int end = a.length -1;
 		
 		while(start <= end) {
+			int mid = start + (end - start)/2;
+			if(a[mid] == target) {
+				return mid;
+			}
+			if(target > a[mid]) {
+				start = mid + 1;
+			}else {
+				end = mid - 1;
+			}
 			
 		}
+		return a[end];
 	}
 
 }
