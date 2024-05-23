3721ee8f891e8b07524823fb2718d7586312be2b
Naveen-13 <naveenbhat5341@gmail.com>
2024-05-23 20:11:53 +0530

    core java

---
+++ src/test/java/Inheritance/BoxWeight.java
@@ -0,0 +1,11 @@
+package Inheritance;
+
+public class BoxWeight extends Box{
+    double weight;
+
+    public BoxWeight(double l, double h, double w, double weight){
+        super(l, h, w);
+        this.weight = weight;
+        
+    }
+}
