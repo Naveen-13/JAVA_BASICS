eef3d737cc4d70557b87b3ddbae88edf78751f55
Naveen-13 <naveenbhat5341@gmail.com>
2024-07-01 21:03:59 +0530

    added increment question

--- src/test/java/collection/test.java
+++ src/test/java/collection/test.java
@@ -1,12 +1,14 @@
 package collection;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class test {
     public static void main(String[] args) {
         String a = "Hello";
-        int[] arr = {13,14,1,4,5,6,7};
+        int[] arr = { 13, 14, 1, 4, 5, 6, 7 };
         test.getCharacters(a);
         test.swapNum(12345);
         test.palindrome(1213);
@@ -14,95 +16,126 @@ public class test {
         test.secondLargest(arr);
         test.vowel("Hello world");
         test.abced("abcd");
+        System.out.println(test.incrementeachdigit(4567));
+        System.out.println(test.increment(4532));
     }
 
-    public static void getCharacters(String a){
+    public static void getCharacters(String a) {
 
         HashMap<Character, Integer> countMap = new HashMap<>();
-        for(char c: a.toCharArray()){
-            countMap.put(c, countMap.getOrDefault(c, 0)+ 1);
+        for (char c : a.toCharArray()) {
+            countMap.put(c, countMap.getOrDefault(c, 0) + 1);
         }
-        for(HashMap.Entry<Character, Integer> entry: countMap.entrySet()){
+        for (HashMap.Entry<Character, Integer> entry : countMap.entrySet()) {
             System.out.println(entry.getKey() + " " + entry.getValue());
         }
     }
 
-    public static void swapNum(int n){
+    public static void swapNum(int n) {
         int rev = 0;
-        while(n!=0){
-            rev = rev*10 + n%10;
-            n=n/10;
+        while (n != 0) {
+            rev = rev * 10 + n % 10;
+            n = n / 10;
         }
         System.out.println(rev);
     }
 
-    public static void palindrome(int n){
+    public static void palindrome(int n) {
         int temp = n;
         int rev = 0;
-        while(n!=0){
-            rev = rev*10 + n%10;
-            n = n/10;
+        while (n != 0) {
+            rev = rev * 10 + n % 10;
+            n = n / 10;
         }
-        if(temp==rev)System.out.println(temp + " Number is palindrome");
-        else System.out.println(temp + " Number is not a palindrome");
+        if (temp == rev)
+            System.out.println(temp + " Number is palindrome");
+        else
+            System.out.println(temp + " Number is not a palindrome");
     }
 
-    public static void palindromeString(String a){
+    public static void palindromeString(String a) {
         String rev = "";
-        for(int i=a.length()-1; i>=0; i--){
+        for (int i = a.length() - 1; i >= 0; i--) {
             rev = rev + a.charAt(i);
         }
-        if(a.equals(rev))System.out.println(a + " is palindrome");
-        else System.out.println(a + " is not a palindrome");
+        if (a.equals(rev))
+            System.out.println(a + " is palindrome");
+        else
+            System.out.println(a + " is not a palindrome");
 
     }
 
-    public static void secondLargest(int[] a){
-		int max = Integer.MIN_VALUE;
-		int secondMax = Integer.MIN_VALUE;
-		for(int num: a){
-            if(num>max){
+    public static void secondLargest(int[] a) {
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+        for (int num : a) {
+            if (num > max) {
                 secondMax = max;
                 max = num;
-        }  
-        else if(num>secondMax && num!=max){
-            secondMax = num;    
+            } else if (num > secondMax && num != max) {
+                secondMax = num;
+            }
         }
+        System.out.println(secondMax);
+
     }
-    System.out.println(secondMax);
-					
-	}
-    
-    public static void vowel(String a){
+
+    public static void vowel(String a) {
         Map<Character, Integer> vowelCount = new HashMap<>();
         vowelCount.put('a', 0);
         vowelCount.put('e', 0);
         vowelCount.put('i', 0);
         vowelCount.put('o', 0);
         vowelCount.put('u', 0);
-         a = a.toLowerCase();
-         for(char c : a.toCharArray()){
-            if(vowelCount.containsKey(c)){
-                vowelCount.put(c, vowelCount.get(c)+1);
+        a = a.toLowerCase();
+        for (char c : a.toCharArray()) {
+            if (vowelCount.containsKey(c)) {
+                vowelCount.put(c, vowelCount.get(c) + 1);
             }
-         }
-         for(HashMap.Entry<Character, Integer> entry : vowelCount.entrySet()){
+        }
+        for (HashMap.Entry<Character, Integer> entry : vowelCount.entrySet()) {
             System.out.println(entry.getKey() + " " + entry.getValue());
-         }
-         
+        }
+
     }
 
-    public static void abced(String a){
+    public static void abced(String a) {
         StringBuilder result = new StringBuilder();
-        for(int i=0;i<a.length();i++){
+        for (int i = 0; i < a.length(); i++) {
             char c = a.charAt(i);
-            int repeatcount = i+1;
-            for(int j=0; j<repeatcount; j++){
+            int repeatcount = i + 1;
+            for (int j = 0; j < repeatcount; j++) {
                 result.append(c);
             }
         }
         System.out.println(result.toString());
     }
 
+    public static int incrementeachdigit(int num) {
+        // converting number into string
+        String str = String.valueOf(num);
+        StringBuilder sb = new StringBuilder();
 
+        for (char c : str.toCharArray()) {
+            int incrementvalue = Character.getNumericValue(c) + 1;
+            StringBuilder a =sb.append(incrementvalue);
+            System.out.println(a);
+        }
+        return Integer.parseInt(sb.toString());
+    }
+
+    public static int increment(int num) {
+        String str = String.valueOf(num);
+        ArrayList<Integer> al = new ArrayList<>();
+        for (char c : str.toCharArray()) {
+            int incrementvalue = Character.getNumericValue(c) + 1;
+            al.add(incrementvalue);
+        }
+        int number = 0;
+        for (int als : al) {
+            number = number * 10 + als;
+        }
+        return number;
+
+    }
 }
